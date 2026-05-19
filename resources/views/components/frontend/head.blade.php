153533eeeac1927ee69a8d463806b4ffafd0a22a
unknown <riddhi@matrixbricks.com>
2026-05-19 11:28:30 +0530

    first commit

--- resources/views/components/frontend/head.blade.php
+++ resources/views/components/frontend/head.blade.php
@@ -1,6 +1,6 @@
   <title>2BE Pumping Log</title>
   <meta charset="utf-8">
-  <link rel="icon" type="image/png" href="{{ asset('/frontend/assets/images/favicon.ico') }}" sizes="32x32" />
+  <link rel="icon" href="https://anvayafoundation.com/2be-new-app/public/frontend/assets/images/favicon.ico">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="{{ asset('frontend/assets/css/style.css') }}">
